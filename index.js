c1f621fd38e6b8f8f78e7489a8095f6fccb02e66
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-08-04 01:50:12 +0300

    Refactor js to use classes

--- index.js
+++ index.js
@@ -1,56 +1,79 @@
-let books = [];
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
+// create a class of book.
+let title=document.querySelector(".title");
+let author=document.querySelector(".author");
 const displayContainer = document.querySelector('.container');
-
-const storeData = () => {
-  localStorage.setItem('books', JSON.stringify(books));
-};
-
-const getData = () => JSON.parse(localStorage.getItem('books'));
-
-const removeBook = (book) => {
-  localStorage.removeItem(book);
-};
-
-const addBook = (title, author) => {
-  const book = {
-    title,
-    author,
-  };
-  books.push(book);
-  const div = document.createElement('div');
-  books.forEach((book, index) => {
-    div.innerHTML = `
-              <p>${book.title}</p>
-              <p>${book.author}</p>                                          
-              <button type="button" id="${index}" class="remove">Remove</button>   
-              <hr>`;
-    const removebtn = div.querySelector('.remove');
-    removebtn.addEventListener('click', () => {
-      books = books.filter((el) => el.key !== index);
-      div.remove();
-      storeData();
-      removeBook(book);
+let books=[];
+class book{
+    constructor(title, author){
+        this.author=author;
+        this.title=title;
+    }
+    //Display 
+    dynamic(){
+    const div = document.createElement('div');
+    books.forEach((book, index) => {
+      div.innerHTML = `
+                <p>${book.title}</p>
+                <p>${book.author}</p>                                          
+                <button type="button" id="${index}" class="remove">Remove</button>   
+                <hr>`;
+      const removebtn = div.querySelector('.remove');
+      removebtn.addEventListener('click', () => {
+        books =books.filter((el) => el.key !== index);
+        this.storeData();
+        div.remove();
+      });
     });
-  });
-  storeData();
-  displayContainer.appendChild(div);
-};
+    displayContainer.appendChild(div);
+    }
 
-document.querySelector('.add').addEventListener('click', (event) => {
-  event.preventDefault();
-  if (title.value !== '' && author.value !== '') {
-    addBook(title.value, author.value);
-    document.querySelector('form').reset();
-  }
-});
+    //Add to local storage
+    storeData(){
+        localStorage.setItem('books', JSON.stringify(books));
+      };
 
-const display = () => {
-  if (getData()) {
-    getData().forEach((book) => {
-      addBook(book.title, book.author);
-    });
-  }
-};
-display();
+    //Remove from local storage
+    getData(){
+        JSON.parse(localStorage.getItem('books'));
+    }
+
+    //Add book to the catalogue. 
+
+    addBook(){
+        let mybook=new book(title.value,author.value);      
+        books.push(mybook);
+        this.storeData();
+        document.querySelector('form').reset();
+        this.dynamic();
+    }
+    execute(){
+        document.querySelector('.add').addEventListener('click', (event) => {
+            event.preventDefault();
+            if (title.value !== '' && author.value !== '') {
+              this.addBook();
+            }
+          });
+
+          window.addEventListener('load', () => {
+            const div = document.createElement('div');
+            books.forEach((book, index) => {
+                div.innerHTML = `
+                          <p>${book.title}</p>
+                          <p>${book.author}</p>                                          
+                          <button type="button" id="${index}" class="remove">Remove</button>   
+                          <hr>`;
+                const removebtn = div.querySelector('.remove');
+                removebtn.addEventListener('click', () => {
+                  books = books.filter((el) => el.key !== index);
+                  div.remove();
+                  this.storeData();
+                });
+              });
+              displayContainer.appendChild(div);
+        });
+    }
+}
+
+//Creating an instant of a class.
+let book1=new book(title.value,author.value);
+book1.execute();
