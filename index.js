81eebcb0a9be37c9a6bc4939eaf7ff585f7e20ec
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-08-03 14:54:25 +0300

    Modified js file to use methods

--- index.js
+++ index.js
@@ -1,65 +1,61 @@
-const books = [];
+let books = [];
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const displayContainer = document.querySelector('.container');
-const storage = () => {
+// Function to add to local storage
+const storeData = () => {
   localStorage.setItem('books', JSON.stringify(books));
 };
-const addBtn = document.querySelector('.add');
-addBtn.addEventListener('click', (event) => {
+// Function to get from local storage.
+const getData = () => JSON.parse(localStorage.getItem('books'));
+// function to remove from local storage.
+const removeBook = (book) => {
+  localStorage.removeItem(book);
+};
+// Function to add book object to the books array.
+const addBook = (title, author) => {
+  const book = {
+    title,
+    author,
+  };
+  books.push(book);
+
+  // Dynamic
+  const div = document.createElement('div');
+  books.forEach((book, index) => {
+    div.innerHTML = `
+              <p>${book.title}</p>
+              <p>${book.author}</p>                                          
+              <button type="button" id="${index}" class="remove">Remove</button>   
+              <hr>`;
+
+    // Remove through the remove button
+    const removebtn = div.querySelector('.remove');
+    removebtn.addEventListener('click', () => {
+      books = books.filter((el) => el.key !== index);
+      div.remove();
+      storeData();
+      removeBook(book);
+    });
+  });
+  storeData();
+  displayContainer.appendChild(div);
+};
+// Add button event.
+
+document.querySelector('.add').addEventListener('click', (event) => {
   event.preventDefault();
   if (title.value !== '' && author.value !== '') {
-    const book = {
-      title,
-      author,
-    };
-    books.push(book);
-    const div = document.createElement('div');
-    books.forEach((book, index) => {
-      div.innerHTML = `
-            <p>${book.title.value}</p>
-            <p>${book.author.value}</p>                                          
-            <button type="button" id="${index}" class="remove">Remove</button>   
-            <hr>`;
-      const removebtn = div.querySelector('.remove');
-      removebtn.addEventListener('click', () => {
-        books.splice(index, 1);
-        div.remove();
-        storage();
-      });
-    });
-    storage();
-    displayContainer.appendChild(div);
+    addBook(title.value, author.value);
     document.querySelector('form').reset();
   }
 });
 
-function display() {
-  const items = JSON.parse(localStorage.getItem('books'));
-  if (items) {
-    items.forEach(() => {
-      const book = {
-        title,
-        author,
-      };
-      books.push(book);
-      const div = document.createElement('div');
-      books.forEach((book, index) => {
-        div.innerHTML = `
-                <p>${book.title.value}</p>
-                <p>${book.author.value}</p>                                          
-                <button type="button" id="${index}" class="remove">Remove</button>   
-                <hr>`;
-        const removebtn = div.querySelector('.remove');
-        removebtn.addEventListener('click', () => {
-          books.splice(index, 1);
-          div.remove();
-          storage();
-        });
-      });
-      storage();
-      displayContainer.appendChild(div);
+const display = () => {
+  if (getData()) {
+    getData().forEach((book) => {
+      addBook(book.title, book.author);
     });
   }
-}
+};
 display();
