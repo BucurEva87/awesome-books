9a90adfba7b5b1af13c619de39e672d499992fe0
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-08-03 14:59:24 +0300

    remove comments

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ let books = [];
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const displayContainer = document.querySelector('.container');
-// Function to add to local storage
+
 const storeData = () => {
   localStorage.setItem('books', JSON.stringify(books));
 };
-// Function to get from local storage.
+
 const getData = () => JSON.parse(localStorage.getItem('books'));
-// function to remove from local storage.
+
 const removeBook = (book) => {
   localStorage.removeItem(book);
 };
-// Function to add book object to the books array.
+
 const addBook = (title, author) => {
   const book = {
     title,
     author,
   };
   books.push(book);
-
-  // Dynamic
   const div = document.createElement('div');
   books.forEach((book, index) => {
     div.innerHTML = `
@@ -28,8 +26,6 @@ const addBook = (title, author) => {
               <p>${book.author}</p>                                          
               <button type="button" id="${index}" class="remove">Remove</button>   
               <hr>`;
-
-    // Remove through the remove button
     const removebtn = div.querySelector('.remove');
     removebtn.addEventListener('click', () => {
       books = books.filter((el) => el.key !== index);
@@ -41,7 +37,6 @@ const addBook = (title, author) => {
   storeData();
   displayContainer.appendChild(div);
 };
-// Add button event.
 
 document.querySelector('.add').addEventListener('click', (event) => {
   event.preventDefault();
